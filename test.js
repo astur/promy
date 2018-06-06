e59dbe92dd185fdd0c29826f3610dbc7fca84da0
astur <astur@yandex.ru>
2018-06-06 13:02:18 +0300

    ava instead mocha

--- test.js
+++ test.js
@@ -1,31 +1,29 @@
+const test = require('ava');
 const promy = require('.');
 
 function asyncTest(ok, cb){
-    setTimeout(() => ok ? cb(null, ok) : cb(new Error()), 10);
+    setTimeout(() => ok ? cb(null, ok) : cb(new Error('error')), 10);
 }
 
 const promyTest = promy(asyncTest);
 
-describe('Callback way', () => {
-    it('should call callback(null, result) when wrapped function does the same', (done) => {
-        promyTest(true, (err, res) => {
-            err ? done('false eror is raised') : done();
-        });
-    });
-    it('should call callback(error) when wrapped function does the same', (done) => {
-        promyTest(false, (err, res) => {
-            err ? done() : done('eror is not catched');
-        });
+test.cb('Callback way: ok', t => {
+    promyTest(true, (err, res) => {
+        t.is(err, null);
+        t.is(res, true);
+        t.end();
     });
 });
 
-describe('Promise way', () => {
-    it('should resolve Promise when wrapped function calls callback(null, result)', (done) => {
-        promyTest(true)
-            .then((res)=>done(), (err)=>done('false eror is raised'));
+test.cb('Callback way: error', t => {
+    promyTest(false, (err, res) => {
+        t.is(err.message, 'error');
+        t.is(res, undefined);
+        t.end();
     });
-    it('should reject Promise when wrapped function calls callback(error)', (done) => {
-        promyTest(false)
-            .then((res)=>done('eror is not catched'), (err)=>done());
-    });
+});
+
+test('Promise way', async t => {
+    t.is(await promyTest(true), true);
+    await t.throws(promyTest(false));
 });
