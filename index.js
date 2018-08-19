a620729c453de67c78ad39f83b4600a1da385f31
astur <astur@yandex.ru>
2018-08-19 12:16:25 +0300

    code linted

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 module.exports = function(fn){
     return function(obj, cb){
         if(cb && cb instanceof Function){
-            fn(obj, cb);
-        } else {
-            return new Promise((resolve, reject) => {
-                fn(obj, (err, res) => {
-                    if(err){
-                        reject(err);
-                    } else {
-                        resolve(res);
-                    }
-                });
-            });
+            return fn(obj, cb);
         }
+        return new Promise((resolve, reject) => {
+            fn(obj, (err, res) => {
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            });
+        });
     };
 };
