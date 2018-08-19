5eb1231c6f45c2ab67668f0fd8183973b957fb3f
astur <astur@yandex.ru>
2018-08-19 12:19:21 +0300

    some refactoring

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-module.exports = function(fn){
-    return function(obj, cb){
-        if(cb && cb instanceof Function){
-            return fn(obj, cb);
-        }
-        return new Promise((resolve, reject) => {
-            fn(obj, (err, res) => {
-                if(err){
-                    reject(err);
-                } else {
-                    resolve(res);
-                }
-            });
+module.exports = fn => (obj, cb) => {
+    if(cb && cb instanceof Function){
+        return fn(obj, cb);
+    }
+    return new Promise((resolve, reject) => {
+        fn(obj, (err, res) => {
+            if(err){
+                reject(err);
+            } else {
+                resolve(res);
+            }
         });
-    };
+    });
 };
